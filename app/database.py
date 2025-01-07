7a4ef9bf0d429556ba40e7a814967fec3f432b87
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-01-07 10:49:07 +0300

    описал database

---
+++ app/database.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from sqlalchemy import func, TIMESTAMP, Integer
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+
+from app.config import database_url
+
+# Создание асинхронного движка для подключения к базе данных
+engine = create_async_engine(url=database_url)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session_maker() as session:
+        try:
+            yield session  # Возвращаем сессию для использования
+        except Exception:
+            await session.rollback()  # Откатываем транзакцию при ошибке
+            raise
+        finally:
+            await session.close()  # Закрываем сессию
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True  # Абстрактный базовый класс, чтобы избежать создания отдельной таблицы
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    @classmethod
+    @property
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
