4c290b8a23f8555cf559b940bf04cde999b6f7aa
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-01-09 22:30:54 +0300

    создал функции current_user register_user

--- app/users/auth.py
+++ app/users/auth.py
@@ -9,27 +9,25 @@ from app.users.dao import UserDAO
 
 @connection()
 async def current_user(tg_id: int, session, secret_key: str = None):
+    print(tg_id, secret_key)
     if secret_key:
         rez = await UserDAO.find_one_or_none(session, tg_id=tg_id, secret_key=secret_key)
     else:
         rez = await UserDAO.find_one_or_none(session, tg_id=tg_id)
     return rez
 
-# async def get_users(tg_id: int, session: SessionDep) -> SUser:
-#     return await UserDAO.find_one_or_none(session, tg_id=tg_id)
-
 
 @connection()
-async def register_user(tg_id: int, secret_key, session=Depends(get_session)):
-    user = await current_user(session, tg_id=31278, secret_key=test)
+async def register_user(tg_id: int, session, secret_key: str):
+    print(tg_id, secret_key)
+    user = await current_user(tg_id=tg_id, secret_key=secret_key)
 
     if user:
         return 'Данный пользователь уже зарегистрирован'
+    else:
+        await UserDAO.update(session=session, filter_by={'secret_key': secret_key}, **{'tg_id': tg_id})
+        return 'Пользователь успешно зарегистрирован'
 
-    await UserDAO.update(session, secret_key, **{'tg_id': tg_id})
-    return 'Пользователь успешно зарегистрирован'
 
-
-# session = Depends(get_session)
-test = asyncio.run(current_user(31278))
-print(test)
+# test = asyncio.run(register_user(tg_id=11111, secret_key='test3'))
+# print(test)
