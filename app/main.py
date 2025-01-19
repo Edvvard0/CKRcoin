4710bf5497e96f97c8322714f62e4a120b101574
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-01-19 20:51:52 +0300

    добавил файл для запуска на сервере

--- app/main.py
+++ app/main.py
@@ -63,5 +63,5 @@ async def webhook(request: Request) -> None:
     print("Update processed")
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
