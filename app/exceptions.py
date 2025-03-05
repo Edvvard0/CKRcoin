b667ba4eee444fff6e47828950bf252dcfbd2bf5
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-03-05 21:41:04 +0300

    добавил новый exception и изменил status_code ответов

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,12 +1,22 @@
 from fastapi import status, HTTPException
 
 
-UserNoExistsException = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail='Пользователя с таким тг id не существует'
+UserNoExistsException = lambda tg_id: HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail=f'User with this tg_id {tg_id} no find'
 )
 
-GroupNoExistsException = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail='Группы с таким id не существует'
+GroupNoExistsException = lambda group_id: HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail=f'Group with this group_id {group_id} no find'
+)
+
+TopUsersException = HTTPException(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    detail='Could not get top users'
+)
+
+UsersMyGroupException = HTTPException(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    detail='Could not find users for this group'
 )
